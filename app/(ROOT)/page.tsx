71c154ca6511a09abb7a90595ad687801e3bcc97
Mukhlisbek <mukhlisbekkirgizov@gmail.com>
2025-03-16 15:20:18 +0500

    push all project files

--- app/(ROOT)/page.tsx
+++ app/(ROOT)/page.tsx
@@ -1,11 +1,25 @@
 import Header from "@/components/Header";
 import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user-action";
 import React from "react";
 
-async function Home() {
+async function Home({ searchParams: { id, page } }: SearchParamProps) {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn?.$id });
+  const accountsData = accounts?.data;
+
+  if (!accounts) return;
+
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+  console.log({
+    accountsData,
+    account,
+  });
+
   return (
     <section className="home">
       <div className="home-content">
@@ -17,17 +31,17 @@ async function Home() {
             subtext="Access and manage your account and transactions efficiently"
           />
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.35}
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
         RECENT TRANSACTIONS
       </div>
       <RightSideBar
         user={loggedIn}
-        transactions={[]}
-        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0, 2)}
       />
     </section>
   );
